68b996f7edcaea734e0678c619b61b6240ebfcfc
Zhao Shan Shan <zhaosszju@163.com>
2015-12-18 17:42:09 +0800

    update login page

--- family_web/script/main.js
+++ family_web/script/main.js
@@ -7,37 +7,27 @@ $(document).on("pageinit", "#index", function () {
 	}
 
 	var submit = $('#submit');
-	submit.on("click", function(e) {
+	submit.tap(function(e) {
 		// e.preventDefault();
 		var that = $(this),
 				formData = $("#loginForm").serialize();
+		
 		console.log(formData);
 		$.mobile.loading("show");
 		var defer = $.ajax({
 			type: "POST",
 			url: Constants.PROTOCAL+Constants.HOST+":"+Constants.PORT+Constants.LOGIN_ACTION,
 			data: formData
-			/*success: function(data, status) {
-				alert($.trim(data));
-			},
-			error: function(data, status) {
-				alert(data + ": " + status);
-			}*/
 		});
 		defer.then(function (response) {
 			console.log(response);
-			// $.mobile.loading("hide");
-			// $.mobile.changePage("#home");
-			// $("#home").show();
-			that.href = Constants.PROTOCAL+Constants.HOST+":"+Constants.PORT + "/family_web/index.html";
+			$.mobile.changePage("index.html");
 		}, function (error) {
 			console.log("Error occured: ");
 			console.log(error);
 			alert("Error: check your account/password!");
 			$.mobile.loading("hide");
 	//		$.mobile.changePage("#home");
-//			$("#home").find(".ui-content p").first().text(error);
-	//		$.mobile.loading("hide");
 		});
 	});
 });

3cc874abc528f6b3329d3f26ae8e8d0dc9196de2
ammilly <zhaosszju@gmail.com>
2015-12-20 16:34:10 +0800

    add login.js

---
+++ family_web/script/login.js
@@ -0,0 +1,32 @@
+$(document).on("pageinit", "#index", function() {
+	function _checkLogin(username, password) {
+		var _name = username, _pass = password;
+		alert(_name + "; " + _pass);
+	}
+
+	var submit = $('#submit');
+	submit.tap(function(e) {
+		// e.preventDefault();
+		var that = $(this),
+				formData = $("#loginForm").serialize();
+		
+		
+		console.log(formData);
+		$.mobile.loading("show");
+		var defer = $.ajax({
+			type: "POST",
+			url: Constants.PROTOCAL+Constants.HOST+":"+Constants.PORT+Constants.LOGIN_ACTION,
+			data: formData
+		});
+		defer.then(function (response) {
+			console.log(response);
+			$.mobile.changePage("index.html");
+		}, function (error) {
+			console.log("Error occured: ");
+			console.log(error);
+			alert("Error: check your account/password!");
+			$.mobile.loading("hide");
+	//		$.mobile.changePage("#home");
+		});
+	});
+});
--- family_web/script/main.js
+++ family_web/script/main.js
@@ -1,33 +1,7 @@
-$(document).on("pageinit", "#index", function () {
-	// 
-//	alert("haha");
-	function _checkLogin(username, password) {
-		var _name = username, _pass = password;
-		alert(_name + "; " + _pass);
-	}
-
-	var submit = $('#submit');
-	submit.tap(function(e) {
-		// e.preventDefault();
-		var that = $(this),
-				formData = $("#loginForm").serialize();
-		
-		console.log(formData);
-		$.mobile.loading("show");
-		var defer = $.ajax({
-			type: "POST",
-			url: Constants.PROTOCAL+Constants.HOST+":"+Constants.PORT+Constants.LOGIN_ACTION,
-			data: formData
-		});
-		defer.then(function (response) {
-			console.log(response);
-			$.mobile.changePage("index.html");
-		}, function (error) {
-			console.log("Error occured: ");
-			console.log(error);
-			alert("Error: check your account/password!");
-			$.mobile.loading("hide");
-	//		$.mobile.changePage("#home");
-		});
-	});
+$(document).on("pagebeforecreate", "#index", function () {
+	// 在页面加载前，检查用户是否已经登录，若没有，则跳转到登录页面
+	console.log("check cookie: ");
+	var cookie = document.cookie;
+	console.log(cookie);
+	
 });

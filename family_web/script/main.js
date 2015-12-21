9c5f9405731763e42096ddf5cb5fe3c7bf2fb5d5
ammilly <zhaosszju@gmail.com>
2015-12-21 21:00:37 +0800

    update main.js

--- family_web/script/main.js
+++ family_web/script/main.js
@@ -1,8 +1,9 @@
-$(document).on("pagebeforecreate", "#index", function () {
+$(document).on("pageinit", "#index", function () {
 	// 在页面加载前，检查用户是否已经登录，若没有，则跳转到登录页面
 	
 	console.log("check cookie: ");
 	var cookie = document.cookie;
 	console.log(cookie);
 	
+
 });
